ee85f02d1b13caa7357658233a76c15dc9b9d40e
longkai.zhao <zhaolongkai3k@gmail.com>
2014-11-18 00:01:45 +0800

    init breakfast project spring mvc/freemarker/jooq sql builder/angular dir/jetty server

---
+++ src/test/java/com/bf/test/AppTest.java
@@ -0,0 +1,24 @@
+package com.bf.test;
+
+
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.webapp.WebAppContext;
+
+/**
+ * Created by kkk .
+ */
+public class AppTest {
+    public static void main(String[] args) {
+        Server jettyServer = new Server(8080);
+
+        WebAppContext wah = new WebAppContext();
+        wah.setContextPath("/breakfast");
+        wah.setWar("src/main/webapp");
+        jettyServer.setHandler(wah);
+        try {
+            jettyServer.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
